60d7f425c1abc631ec56adfb4beb66139409a110
Karen <to.karenngo@gmail.com>
2021-11-23 21:44:54 -0500

    added assertEquals

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@ const assertEqual = function(actual,expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`❌ Assertion Failed: ${actual} !=== ${expected}`);
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
@@ -11,3 +11,4 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("cat", "cat");
 assertEqual(1, 6);
+
