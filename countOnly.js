326af9e3f00a2dc4e2ada750eeef84438e742b8c
Karen <to.karenngo@gmail.com>
2021-11-24 20:09:32 -0500

    added itemsToCount portion so it only counts specific ones

--- countOnly.js
+++ countOnly.js
@@ -6,7 +6,7 @@ const assertEqual = function(actual,expected) {
   }
 };
 
-// countOnly will be given an array and an object. It will return an object 
+// countOnly will be given an array and an object. It will return an object
 //containing counts of everything that the input object listed.
 
 // • recognize from the object which elements we should be looking for in the array
@@ -20,17 +20,18 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    if (results[item]){
-        results[item] += 1
-    } else {
-      results[item] = 1
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
-
   return results;
-}
+};
 
-// If results[item] exists count += 1 
+// If results[item] exists count += 1
 // else results[item] = 1
 //if it doesn't exist - undefined = false && if it doesn't exist, we want to assign key:value to object
 //if it does exist it would come back defined = true && if it does exist, we want to increment the existing value in the key:value +1
@@ -48,8 +49,8 @@ for (const item of allItems) {
 */
 
 
-// Functions can only return one thing. 
-// That thing can be a primitive data type, like a number, a boolean, null or a string. 
+// Functions can only return one thing.
+// That thing can be a primitive data type, like a number, a boolean, null or a string.
 // In some cases it makes sense to return an object.
 
 const firstNames = [
@@ -68,7 +69,7 @@ const firstNames = [
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 //you see in the assertEqual lines that it's passing a key:value pair of an object and a number
 //you get the first error because when assertEqual looks for result1, and then result one goes to countOnly, it doesn't see an object to find the key:value pair therefore is ERROR result1/ jason is undefined
-//you fix that first error by returning an empty object in the function countOnly, so now when you run it, the object exists but is undefined. 
+//you fix that first error by returning an empty object in the function countOnly, so now when you run it, the object exists but is undefined.
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
@@ -91,7 +92,7 @@ assertEqual(result1["Agouhanna"], undefined);
 //     at Function.Module._load (node:internal/modules/cjs/loader:813:14)
 //     at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:76:12)
 
-//means there is the a mismatch at result1["Jason"] and so what could that be, this notation is referring to an object, 
+//means there is the a mismatch at result1["Jason"] and so what could that be, this notation is referring to an object,
 //and the code currently doesn't defined result1 as an object.
 
 //you don't need to pass info to all parameters for code to run, ready for future use in function
