3c7223eb541b43a82ebd2063eec07fb4e5719496
Karen <to.karenngo@gmail.com>
2021-11-29 16:17:18 -0500

    refactoring tail.js, moving test code to tailTest.js one directory down, exporting tail, and requiring it in tailTest.js

--- tail.js
+++ tail.js
@@ -1,11 +1,14 @@
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
+// const assertEqual = function(actual,expected) {
+//   if (actual === expected) {
+//     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// }; //removed to refactor and require assert function
 /*
+
 const assertEqual = function (actual,expected) {
   let array = [];
   let tailArray = [];
@@ -26,39 +29,8 @@ const tail = (array) => {
 }
 */
 
-let array = [5,6,7,9,10,11];
 const tail = (array) => {
   return array.slice(1);
 };
 
-console.log(tail([5,6,7,9,10,11]));
-console.log(array);
-//this test case shows that the original array was not modified
-
-
-// Another Test Case: Check the original array was not modified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-//Other Test Cases
-
-//An array with only one element should yield an empty array for its tail
-console.log(tail([5])); //returns []
-
-//An empty array should yield an empty array for its tail
-console.log(tail([])); //returns []
-
-
-
-
-
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 2:
-const final = tail([1,2,3]);
-assertEqual(final[0],2);
+module.exports = tail;
