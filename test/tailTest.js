4db58236f39c9b37b8a3a0c8d561cc1db3e4a7fa
Karen <to.karenngo@gmail.com>
2021-11-29 18:39:59 -0500

    using chai's assert and writing mocha test code for tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,38 +1,38 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
 
-let array = [5,6,7,9,10,11];
-console.log(tail([5,6,7,9,10,11]));
-console.log(array);
-//this test case shows that the original array was not modified
-
-
-// Another Test Case: Check the original array was not modified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-//Other Test Cases
-
-// //An array with only one element should yield an empty array for its tail
-// console.log(tail([5])); //returns []
-
-// //An empty array should yield an empty array for its tail
-// console.log(tail([])); //returns []
+// let array = [5,6,7,9,10,11];
+// console.log(tail([5,6,7,9,10,11]));
+// console.log(array);
+// //this test case shows that the original array was not modified
 
 
+// // Another Test Case: Check the original array was not modified
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// //Other Test Cases
+
+// // //An array with only one element should yield an empty array for its tail
+// // console.log(tail([5])); //returns []
+
+// // //An empty array should yield an empty array for its tail
+// // console.log(tail([])); //returns []
 
 
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-// Test Case 2:
-const final = tail([1,2,3]);
-assertEqual(final[0],2);
+
+// // Test Case 1: Check the returned array elements
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// // Test Case 2:
+// const final = tail([1,2,3]);
+// assertEqual(final[0],2);
 
 
 
@@ -40,22 +40,10 @@ const assert = require('chai').assert;
 const tail   = require('../tail');
 
 describe("#tail", () => {
-  it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(head([1, 2, 3]), 1);
+  it('returns ["Lighthouse","Lab"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]),["Lighthouse", "Labs"]);
   });
-  it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
-  });
-  it("returns 5 for [5 ,6 , 7]", () => {
-    assert.strictEqual(head([5, 6, 7]), 5); 
-  });
-  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
-    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
-  });
-  it("returns 6 for [6]", () => {
-    assert.strictEqual(head([6]), 6); 
-  });
-  it("returns undefined for []", () => {
-    assert.strictEqual(head([]), undefined); 
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]); 
   });
 });
